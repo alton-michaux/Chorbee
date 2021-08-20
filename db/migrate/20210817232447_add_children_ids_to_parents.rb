5f66b8eba584c9a8542de299764c7eb67e920c71
Alton Michaux <alton.michaux@yahoo.com>
2021-08-20 14:39:16 -0400

    <bugfix>used .uniq method to irradicate duplicates within current_parent arrays

--- db/migrate/20210817232447_add_children_ids_to_parents.rb
+++ db/migrate/20210817232447_add_children_ids_to_parents.rb
@@ -2,6 +2,6 @@
 
 class AddChildrenIdsToParents < ActiveRecord::Migration[5.0]
   def change
-    add_index(:parents, %i[children_ids parent_id], unique: false)
+    add_index(:parents, %i[children_ids parent_id], unique: true)
   end
 end
