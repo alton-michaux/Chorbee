177d676c334bb2b41999b72285769477faa10749
Alton Michaux <alton.michaux@yahoo.com>
2022-05-28 01:16:46 -0400

    finished postgre integration and all relations should be correct (more or less) will tweak and keep sqlite version on seperate branch just in case

--- db/migrate/20220528041721_create_join_table_chore_child.rb
+++ db/migrate/20220528041721_create_join_table_chore_child.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableChoreChild < ActiveRecord::Migration[5.0]
   def change
     create_join_table :chores, :children do |t|
-      # t.index [:chore_id, :child_id]
-      # t.index [:child_id, :chore_id]
+      t.index [:chore_id, :child_id]
+      t.index [:child_id, :chore_id]
     end
   end
 end
